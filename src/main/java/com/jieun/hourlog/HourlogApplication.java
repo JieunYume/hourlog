d068968d81807c97c57d18a71440bf11603b5fdb
이지은 <jieun7056@naver.com>
2026-05-02 00:08:42 +0900

    init

---
+++ src/main/java/com/jieun/hourlog/HourlogApplication.java
@@ -0,0 +1,13 @@
+package com.jieun.hourlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HourlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jieun/hourlog/HourlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jieun.hourlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
